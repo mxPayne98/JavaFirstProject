0daff50d645389098c694ca99a3ec3a1f1f36a41
mxPayne98 <bhavyamkark98@gmail.com>
2020-05-03 11:36:58 +0530

    task3 modified

--- Task/src/main/java/com/sapient/week4/Num_Process_Runner.java
+++ Task/src/main/java/com/sapient/week4/Num_Process_Runner.java
@@ -10,27 +10,20 @@ public class Num_Process_Runner {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int choice = sc.nextInt();
-            int[] answers = new int[4];
-            Num_Process add = (int n1, int n2) -> n1 + n2;
-            answers[0] = add.call(a, b);
+            Num_Process[] answers = new Num_Process[4];
 
-            Num_Process subtract = (int n1, int n2) -> n1 - n2;
-            answers[1] = subtract.call(a, b);
-
-            Num_Process multiply = (int n1, int n2) -> n1 * n2;
-            answers[2] = multiply.call(a, b);
-
-            Num_Process divide = (int n1, int n2) -> n1 / n2;
-            answers[3] = divide.call(a, b);
+            answers[0] = (int n1, int n2) -> n1 + n2;
+            answers[1] = (int n1, int n2) -> n1 - n2;
+            answers[2] = (int n1, int n2) -> n1 * n2;
+            answers[3] = (int n1, int n2) -> n1 / n2;
 
             try {
-                System.out.println(answers[choice]);
+                System.out.println(answers[choice].call(a,b));
             } catch (ArrayIndexOutOfBoundsException e) {
                 e.printStackTrace();
             }
 
             answers = null;
-            add = subtract = multiply = divide = null;
         }
 
     }
